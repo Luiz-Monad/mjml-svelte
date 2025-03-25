f2bd9108a5c9658cf27651928ac322332bd435fc
Luiz Felipe Stangarlin <luizfelipestang@gmail.com>
2025-03-25 13:56:15 -0500

    fix: combine tag style inside mj-style

--- src/utils/mjml.ts
+++ src/utils/mjml.ts
@@ -1,6 +1,6 @@
 import { ElementType, parseDocument } from 'htmlparser2';
 import { render } from 'dom-serializer';
-import { type Node, Element, type Document, Text, isTag } from 'domhandler';
+import { type Node, Element, type Document, Text, isTag, hasChildren } from 'domhandler';
 
 export type XmlDocument = Document;
 
@@ -16,10 +16,17 @@ export function isElement(node: Node): node is Element {
   return isTag(node);
 }
 
-export function findChildByTagName(parent: Element, tagName: string): Element | undefined {
-  return parent.children.find(
-    (child): child is Element => isElement(child) && child.tagName === tagName
-  );
+export function findOneByTagName(parent: Element, tagName: string): Element | undefined {
+  return parent.children.filter(isElement).find((child) => child.tagName === tagName);
+}
+
+export function findChildByTagName(parent: Element, tagName: string): Element[] {
+  return parent.children
+    .filter(isElement)
+    .flatMap((child) => [
+      ...(hasChildren(child) ? findChildByTagName(child, tagName) : []),
+      ...(child.tagName === tagName ? [child] : [])
+    ]);
 }
 
 export function createChildTag(parent: Element, tagName: string): Element {
@@ -35,9 +42,22 @@ export function createChildText(parent: Element, text: string): Text {
 }
 
 export function getOrCreateChildTag(parent: Element, tagName: string): Element {
-  const existingChild = findChildByTagName(parent, tagName);
+  const existingChild = findOneByTagName(parent, tagName);
   if (existingChild) {
     return existingChild;
   }
   return createChildTag(parent, tagName);
+}
+
+export function moveAllChild(fromParent: Element, toParent: Element) {
+  const items = fromParent.children;
+  fromParent.children = [];
+  for (const item of items) {
+    item.parent = toParent;
+  }
+  toParent.children.push(...items);
+}
+
+export function removeChild(parent: Element, child: Element) {
+  parent.children = Array.from(parent.children.filter((c) => c != child));
 }
